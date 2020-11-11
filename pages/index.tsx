0b5e150f0a4bdc98d669040cfb6eeaa6611a3216
Christopher Wattaul <chris.wattaul@gmail.com>
2020-11-11 14:55:09 +0100

    Modify index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { initializeApollo } from '../lib/apollo';
-import { useViewerQuery, ViewerDocument } from '../lib/viewer.graphql';
+import { ViewerDocument } from '../lib/viewer.graphql';
 import CanDoForYou from '../src/components/CanDoForYou';
 import Intro from '../src/components/Intro';
 import Page from '../src/components/Page';
 
 const Index = () => {
-  const { viewer } = useViewerQuery().data!;
+  // const { viewer } = useViewerQuery().data!;
 
   return (
     <Page>
