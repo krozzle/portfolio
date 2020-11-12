12af7e919853ab01c5baf56615452edf0ada71c6
Christopher Wattaul <chris.wattaul@gmail.com>
2020-11-12 16:11:10 +0100

    Add styling

--- src/components/CanDoForYou.tsx
+++ src/components/CanDoForYou.tsx
@@ -1,14 +1,16 @@
 const CanDoForYou = () => (
-  <div className="flex sm:flex-col md:flex-row flex-wrap min-h-screen items-center justify-around snap-mandatory bg-teal-600 py-24">
-    <p className="md:w-1/3 px-12 py-20 border-b-2 lg:border-r-2  border-gray-500 text-gray-300 text-5xl md:text-right uppercase font-extrabold">
-      what i can do <br /> for you
+  <div className="flex sm:flex-col lg:flex-row flex-wrap min-h-screen items-center justify-around snap-mandatory bg-teal-600 py-24">
+    <p className="lg:w-1/3 px-12 py-12 lg:py-20 border-b-2 lg:border-r-2  border-gray-500 text-gray-300 text-4xl lg:text-right uppercase font-extrabold">
+      what i can do <br className="lg:hidden xl:inline" /> for you
     </p>
-    <ul className="md:w-2/3 px-24 leading-loose list-none text-3xl">
-      <li>Create </li>
-      <li>Create </li>
-      <li>Create </li>
-      <li>Create </li>
-    </ul>
+    <div className="lg:w-2/3 px-24 leading-loose list-none text-3xl">
+      <ul>
+        <li>Create </li>
+        <li>Create </li>
+        <li>Create </li>
+        <li>Create </li>
+      </ul>
+    </div>
   </div>
 );
 
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,19 +1,28 @@
 const Intro = () => (
-  <div className="flex justify-around py-24">
+  <div className="flex flex-col lg:flex-row justify-around py-24 bg-gray-200">
     <div className="w-1/2">
       <img
         src="/kriz-cv.jpg"
         alt="picture of krozzle posing for a new profile picture"
-        className="w-2/3 ml-auto px-12 shadow-sm rounded-tl-full rounded-br-full"
+        className="lg:w-2/3 ml-auto px-12 py-12 shadow-sm rounded-tl-full rounded-br-full"
       />
     </div>
-    <div className="w-1/2 mr-auto px-12 flex flex-col justify-center align-middle">
-      <div className="text-4xl py-2 text-gray-900 font-extrabold tracking-normal">
-        Coovfefe, I'm Chris
-      </div>
-      <div className="text-2xl text-gray-800">Let's turn your</div>
-      <div className="text-2xl text-gray-800">Ideas into</div>
-      <div className="text-3xl text-gray-500 font-serif font-bold">Reality</div>
+    <div className="w-1/2 mr-auto px-12 flex flex-col justify-center items-start">
+      <h1 className="text-4xl text-gray-800 font-bold tracking-wide">
+        Chris Wattaul
+      </h1>
+      <h1 className="text-4xl text-indigo-500 font-extrabold tracking-wide">
+        Frontend Developer
+      </h1>
+      <p className="text-3xl font-semibold leading-loose text-gray-800">
+        Let's turn your
+      </p>
+      <p className="text-3xl font-semibold leading-loose text-gray-800">
+        Ideas into
+      </p>
+      <p className="text-3xl leading-loose text-gray-500 font-serif font-bold">
+        Reality
+      </p>
     </div>
   </div>
 );
