7aa13b07567efd83ea5ffd2a5e17c9212fad3d42
Christopher Wattaul <chris.wattaul@gmail.com>
2020-11-13 17:20:11 +0100

    Add menu svg files, styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 const Header = () => (
   <div className="flex justify-around py-4 text-md text-gray-800 font-hairline opacity-25 transition duration-500 hover:ease-in-out hover:opacity-100 bg-gray-500 hover:bg-opacity-50 top-0 sticky">
     <div className="flex flex-1 justify-around">
-      <div>chris wattaul</div>
+      <div>
+        <a href="/">chris wattaul</a>
+      </div>
     </div>
-    <div className="flex flex-1 justify-center">
+    <div className="hidden lg:flex hd:flex-1 lg:justify-center">
       <div className="">
         <a href="/" className="px-2">
           work
@@ -18,6 +20,13 @@ const Header = () => (
       <div className="flex justify-center px-4 w-1/3">
         <button className=" rounded-lg px-2 py-1 transition duration-300 focus:ease-in-out bg-gray-200 focus:bg-gray-700 bg-opacity-25 focus:bg-opacity-25 focus:outline-none">
           darkMode
+        </button>
+      </div>
+    </div>
+    <div>
+      <div className="lg:hidden flex justify-end items-center pr-12">
+        <button type="button" className="fill-current gray-700-to-current">
+          <img src="/menu.svg" alt="menu button" />
         </button>
       </div>
     </div>
