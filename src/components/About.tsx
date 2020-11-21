73684e22969dfc61bff12534782644635a3add0a
Christopher Wattaul <chris.wattaul@gmail.com>
2020-11-21 12:21:03 +0100

    Add styling, create AboutMobile

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,11 @@
+import AboutMobile from './utils/AboutMobile';
+
 const About = () => (
   <div
     id="about"
-    className="snap snap-start flex flex-col-reverse lg:flex-row justify-center items-center min-h-screen  bg-gray-700"
+    className="flex flex-col-reverse lg:flex-row justify-center items-center min-h-screen bg-gray-100"
   >
-    <div className="hidden lg:flex lg:flex-col w-full lg:w-2/3 xl:py-32 px-8 xl:px-16 xl:mx-auto xl:max-w-6xl text-3xl text-center text-gray-500 lg:text-right tracking-wider lg:tracking-normal leading-18">
+    <div className="hidden lg:flex lg:flex-col w-full lg:w-2/3 xl:py-32 px-8 xl:px-16 xl:mx-auto xl:max-w-6xl text-3xl text-center text-gray-800 lg:text-right tracking-wider lg:tracking-normal leading-18">
       <p>
         Born and raised in Vienna, Austria, <br />
         of mixed descent, there's no other place <br /> I want to call home.
@@ -56,54 +58,8 @@ const About = () => (
         .
       </p>
     </div>
-    <div className="lg:hidden flex flex-col justify-center items-center py-4 text-4xl text-gray-500 font-hairline leading-relaxed tracking-wide">
-      <p>Vienna 🇦🇹 🇵🇭</p>
-      <p>12+ years 👩‍⚕️</p>
-      <p>
-        <a
-          href="https://upleveled.io"
-          className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
-        >
-          UpLeveled
-        </a>{' '}
-        💻
-      </p>
-      <p>
-        <a
-          href="https://github.com/krozzle"
-          className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
-        >
-          Sharing is caring
-        </a>{' '}
-        😻
-      </p>
-      <p>
-        <a
-          href="https://nextjs.org"
-          className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
-        >
-          Next.js
-        </a>
-        ,{' '}
-        <a
-          href="https://graphql.org"
-          className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
-        >
-          GraphQL
-        </a>{' '}
-        ⚛
-      </p>
-      <p>
-        <a
-          href="https://virtualdojovienna.com"
-          className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
-        >
-          Virtual Dojo Vienna
-        </a>{' '}
-        🕹
-      </p>
-    </div>
-    <div className="flex justify-center items-center p-1 lg:items-end lg:w-1/3 text-6xl font-semibold text-gray-300 border-b-2 border-vdvPink tracking-wide">
+    <AboutMobile />
+    <div className="flex justify-center items-center p-1 lg:items-end lg:w-1/3 text-6xl font-semibold text-gray-800 border-b-2 border-vdvPink tracking-wide">
       <p>About me</p>
     </div>
   </div>
---
+++ src/components/utils/AboutMobile.tsx
@@ -0,0 +1,51 @@
+const AboutMobile = () => (
+  <div className="lg:hidden flex flex-col justify-center items-center py-4 text-4xl text-gray-800 font-hairline leading-relaxed tracking-wide">
+    <p>Vienna 🇦🇹 🇵🇭</p>
+    <p>12+ years 👩‍⚕️</p>
+    <p>
+      <a
+        href="https://upleveled.io"
+        className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
+      >
+        UpLeveled
+      </a>{' '}
+      💻
+    </p>
+    <p>
+      <a
+        href="https://github.com/krozzle"
+        className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
+      >
+        Sharing is caring
+      </a>{' '}
+      😻
+    </p>
+    <p>
+      <a
+        href="https://nextjs.org"
+        className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
+      >
+        Next.js
+      </a>
+      ,{' '}
+      <a
+        href="https://graphql.org"
+        className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
+      >
+        GraphQL
+      </a>{' '}
+      ⚛
+    </p>
+    <p>
+      <a
+        href="https://virtualdojovienna.com"
+        className="underline rounded-md md:hover:bg-vdvPink md:hover:bg-opacity-50"
+      >
+        Virtual Dojo Vienna
+      </a>{' '}
+      🕹
+    </p>
+  </div>
+);
+
+export default AboutMobile;
