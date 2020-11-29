cf8fb5d74eb589393f46ae6feac0d6012dd763dc
Christopher Wattaul <chris.wattaul@gmail.com>
2020-11-29 19:23:27 +0100

    Modify header, add images

---
+++ src/components/utils/NavLinks.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+
+const navLinks = [
+  {
+    title: 'Work',
+    href: '#work',
+  },
+  {
+    title: 'Skills',
+    href: '#skills',
+  },
+  {
+    title: 'About',
+    href: '#about',
+  },
+];
+
+const NavLinks = () => (
+  <div className="w-full md:w-2/3 h-24 flex justify-around items-center bg-indigo-900 text-xl">
+    <div>
+      <a href="/">Chris Wattaul</a>
+    </div>
+
+    <ul className="flex justify-around items-center text-lg">
+      {navLinks.map((nav, i) => (
+        <Link href={nav.href}>
+          <a>
+            <li key={i} className="px-4">
+              {nav.title}
+            </li>
+          </a>
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
+
+export default NavLinks;
