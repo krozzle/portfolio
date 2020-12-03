460dc29813b0df7ea270989adec2928a00de124f
Christopher Wattaul <chris.wattaul@gmail.com>
2020-12-03 19:11:14 +0100

    Add animations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,33 @@
+import Fade from 'react-reveal/Fade';
+
 const Footer = () => {
   const fullYear = new Date().getFullYear();
 
   return (
-    <div className="flex justify-around lg:justify-start lg:px-32 items-center py-4 bg-gray-300 text-md text-lg text-gray-900 bottom-0 lg:sticky">
-      <div className="flex lg:hidden space-x-4">
-        <a href="https://github.com/krozzle">
-          <img src="/github.svg" />
-        </a>
-        <a href="https://twitter.com/krozzler">
-          <img src="/twitter.svg" />
-        </a>
-        <a href="https://linkedin.com/in/christopher-wattaul/">
-          <img src="/linkedin.svg" />
-        </a>
+    <Fade>
+      <div className="flex justify-around lg:justify-start lg:px-32 items-center py-4 bg-gray-300 text-md text-lg text-gray-900 bottom-0 lg:sticky">
+        <div className="flex lg:hidden space-x-4">
+          <a href="https://github.com/krozzle">
+            <img src="/github.svg" />
+          </a>
+          <a href="https://twitter.com/krozzler">
+            <img src="/twitter.svg" />
+          </a>
+          <a href="https://linkedin.com/in/christopher-wattaul/">
+            <img src="/linkedin.svg" />
+          </a>
+        </div>
+        <div className="flex">
+          <div>Ⓒ {fullYear} •</div>
+          <a
+            href="/legalNotice"
+            className="hover:text-vdvPink transition-colors duration-300"
+          >
+            <span className="px-2">Legal Notice</span>
+          </a>
+        </div>
       </div>
-      <div className="flex">
-        <div>Ⓒ {fullYear} •</div>
-        <a
-          href="/legalNotice"
-          className="hover:text-vdvPink transition-colors duration-300"
-        >
-          <span className="px-2">Legal Notice</span>
-        </a>
-      </div>
-    </div>
+    </Fade>
   );
 };
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
+import Fade from 'react-reveal/Fade';
 import NavLinks from './utils/NavLinks';
 import SocialLinks from './utils/SocialLinks';
 
 const Header = () => (
-  <div className="flex items-center text-gray-100 font-medium tracking-wide top-0 sticky z-10">
-    <NavLinks />
-    <SocialLinks />
-  </div>
+  <Fade>
+    <div className="flex items-center text-gray-100 font-medium tracking-wide top-0 sticky z-10">
+      <NavLinks />
+      <SocialLinks />
+    </div>
+  </Fade>
 );
 
 export default Header;
