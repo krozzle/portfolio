6596d0849786dea33e1f962ddb4b863f76f5f873
Christopher Wattaul <chris.wattaul@gmail.com>
2020-12-03 10:45:49 +0100

    Add styling

--- src/components/utils/NavLinks.tsx
+++ src/components/utils/NavLinks.tsx
@@ -3,36 +3,38 @@ import Link from 'next/link';
 const navLinks = [
   {
     title: 'Work',
-    section: '/',
+    section: '#work',
+    ref: 'workRef',
   },
   {
     title: 'Skills',
     section: '#skills',
+    ref: 'skillsRef',
   },
   {
     title: 'About',
     section: '#about',
+    ref: 'aboutRef',
   },
 ];
 
-const NavLinks = () => (
-  <div className="w-full md:w-2/3 h-24 flex justify-between lg:justify-between items-center px-12 lg:px-32 bg-indigo-900 text-xl">
-    <div className="">
-      <a href="/">Chris Wattaul</a>
+const NavLinks = () => {
+  return (
+    <div className="w-full md:w-2/3 h-24 flex justify-between lg:justify-between items-center px-12 lg:px-32 bg-indigo-900 text-xl">
+      <div className="">
+        <a href="/">Chris Wattaul</a>
+      </div>
+
+      <ul className="flex justify-around items-center space-x-4 text-lg">
+        {navLinks.map((nav, i) => (
+          <Link href={nav.section} key={i}>
+            <a ref={nav.ref}>
+              <li key={i}>{nav.title}</li>
+            </a>
+          </Link>
+        ))}
+      </ul>
     </div>
-
-    <ul className="flex justify-around items-center text-lg">
-      {navLinks.map((nav, i) => (
-        <Link href={nav.section} key={i}>
-          <a>
-            <li key={i} className="pr-4">
-              {nav.title}
-            </li>
-          </a>
-        </Link>
-      ))}
-    </ul>
-  </div>
-);
-
+  );
+};
 export default NavLinks;
